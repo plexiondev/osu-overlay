9c225fcf817e0c3002ef53b32a2cfae60b2692c7
plexion <46572320+plexiondev@users.noreply.github.com>
2022-06-11 19:02:58 +0100

    create js

--- gosumemory/plexion/js/index.js
+++ gosumemory/plexion/js/index.js
@@ -3,3 +3,32 @@
 */
 
 
+// socket
+let socket = new ReconnectingWebSocket("ws://127.0.0.1:24050/ws");
+// connect
+socket.onopen = () => {
+    console.log('Connected');
+};
+// on exit
+socket.onclose = event => {
+    console.log(`Closed connection: ${event}`);
+    socket.send('Client closed!');
+};
+// on error
+socket.onerror = error => {
+    console.log(`Encountered error: ${error}`);
+};
+
+
+// grades
+let grade = document.getElementById('grade');
+function grade(text,colour) {
+    grade.textContent = text;
+    grade.style.colour = colour;
+}
+
+
+// main loop
+socket.onmessage = event => {
+    let data = JSON.parse(event.data);
+}
